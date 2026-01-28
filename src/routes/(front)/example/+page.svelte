addfd04de79df1cd8bb90cab275242e8e7754a85
GuillianV <guillianvibert90@gmail.com>
2026-01-28 16:41:18 +0100

    feat: parent references

--- src/routes/(front)/example/+page.svelte
+++ src/routes/(front)/example/+page.svelte
@@ -2,6 +2,9 @@
 	// @ts-nocheck
 	let { data } = $props();
 	const { example, fullpath, paths, filename } = data;
+	const { asset, references } = example;
+
+	let references_tab_opened = $state(false);
 </script>
 
 <div class="bg-neutral-100 p-8 w-full h-fit">
@@ -18,9 +21,40 @@
 		</h1>
 	</div>
 
+	{#if references.references}
+		<!-- svelte-ignore a11y_click_events_have_key_events -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+		<div class="mt-4 rounded-3xl border h-auto border-black p-8 overflow-x-auto">
+			<!-- svelte-ignore a11y_click_events_have_key_events -->
+			<!-- svelte-ignore event_directive_deprecated -->
+			<h2
+				class="font-bold text-3xl cursor-pointer underline"
+				on:click={() => (references_tab_opened = !references_tab_opened)}
+			>
+				Parents references
+			</h2>
+			<div class="flex flex-col {references_tab_opened ? 'hidden' : 'block'}">
+				{filename} : {'{'}
+
+				{#each references.references.sort() as reference}
+					<a
+						href="/example?fullpath={reference}"
+						target="_blank"
+						class="w-fit h-fit cursor-pointer mt-2 ml-8 p-2 bg-neutral-200 hover:bg-neutral-300"
+					>
+						{reference} : {'{'}...{'}'}
+					</a>
+				{/each}
+
+				{'}'}
+			</div>
+		</div>
+	{/if}
+
 	<div class="rounded-3xl w-full relative p-8 border mt-8 border-black overflow-x-auto">
 		<pre><code>
-{JSON.stringify(example, null, 4)}
+{JSON.stringify(asset, null, 4)}
 		</code></pre>
 	</div>
 </div>
